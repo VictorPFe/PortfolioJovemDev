9691abf43f4780646cb4acf3a8fbc12622a5d49a
VictorPFe <victorpessoa462@gmail.com>
2025-10-30 19:01:22 -0300

    Atualização na NavBar Margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { SobreMim } from "../_components/SobreMim";
 import { Contato } from "@/_components/Contato";
 import { Habilidades } from "@/_components/Habilidades";
 import { Projetos } from "@/_components/Projetos";
-import { Footer } from "@/_components/Footer";
+import { Footer } from "@/_components/footer";
 
 export default function index(){
   return (
