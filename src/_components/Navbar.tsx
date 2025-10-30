1f7d5fb3c7c3b87c358557dd310f890714c40496
VictorPFe <victorpessoa462@gmail.com>
2025-10-30 16:39:35 -0300

    Atualização na Navbar - Menu Mobile Atualização

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import JD from "../../public/logo/JD.svg";
 import { List, X } from "@phosphor-icons/react/dist/ssr";
@@ -8,6 +9,15 @@ import Link from "next/link";
 
 export function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const pathname = usePathname(); // Pega a rota atual
+
+  const links = [
+    {href: "/#", label: "Home"},
+    {href: "/#sobremim", label: "Sobre mim"},
+    {href: "/#contatos", label: "Contatos"},
+    {href: "/#habilidades", label: "Habilidades"},
+    {href: "/#projetos", label: "Projetos"},
+  ]
 
   return (
     <header className="w-full fixed top-0 z-11">
@@ -31,31 +41,20 @@ export function Navbar() {
         {/* LINKS - VISÍVEL APENAS EM TELAS GRANDES */}
         <nav className="text-white font-[family-name:var(--interSans)] font-bold xl:flex hidden">
           <ul className="flex gap-7">
-            <li>
-              <Link href="#" className="hover:text-[#38E1C6]">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="#sobremim" className="hover:text-[#38E1C6]">
-                Sobre mim
-              </Link>
-            </li>
-            <li>
-              <Link href="#contatos" className="hover:text-[#38E1C6]">
-                Contatos
-              </Link>
-            </li>
-            <li>
-              <Link href="#habilidades" className="hover:text-[#38E1C6]">
-                Habilidades
-              </Link>
-            </li>
-            <li>
-              <Link href="#projetos" className="hover:text-[#38E1C6]">
-                Projetos
-              </Link>
-            </li>
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link 
+                  href={link.href}
+                  className={`transition-colors ${
+                    pathname === link.href
+                      ? "text-[#38E1C6]" // Ativo
+                      : "hover:text-[#38E1C6]" // hover
+                  }`}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
@@ -89,46 +88,20 @@ export function Navbar() {
       {/* MENU MOBILE */}
       {menuOpen && (
         <div className="rounded-b-sm xl:hidden bg-[#141C27] font-[family-name:var(--interSans)] text-white font-bold flex flex-col items-center py-6 space-y-5 shadow-lg mx-10 md:mx-20 lg:mx-50 ">
-          <Link
-            rel="stylesheet"
-            href="#"
-            onClick={() => setMenuOpen(false)}
-            className="hover:text-[#38E1C6]"
-          >
-            Home
-          </Link>
-          <Link
-            rel="stylesheet"
-            href="#sobremim"
-            onClick={() => setMenuOpen(false)}
-            className="hover:text-[#38E1C6]"
-          >
-            Sobre mim
-          </Link>
-          <Link
-            rel="stylesheet"
-            href="#contatos"
-            onClick={() => setMenuOpen(false)}
-            className="hover:text-[#38E1C6]"
-          >
-            Contato
-          </Link>
-          <Link
-            rel="stylesheet"
-            href="#habilidades"
-            onClick={() => setMenuOpen(false)}
-            className="hover:text-[#38E1C6]"
-          >
-            Habilidades
-          </Link>
-          <Link
-            rel="stylesheet"
-            href="#projetos"
-            onClick={() => setMenuOpen(false)}
-            className="hover:text-[#38E1C6]"
-          >
-            Projetos
-          </Link>
+          {links.map((link) => (
+              <li key={link.href}>
+                <Link 
+                  href={link.href}
+                  className={`transition-colors ${
+                    pathname === link.href
+                      ? "text-[#38E1C6]" // Ativo
+                      : "hover:text-[#38E1C6]" // hover
+                  }`}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
            <a
             href="/Curriculo/Curriculo-VictorPessoa.pdf"
             download="Curriculo-Victor-Pessoa"
